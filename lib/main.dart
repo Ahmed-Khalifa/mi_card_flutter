7703de8aafcfce97faef7cea69a6e0bae3db737c
Ahmed Khalifa <ahmedkhalifa090@gmail.com>
2020-11-16 18:21:22 +0300

    Finished the Challenge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,45 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                  Container(
+                    color: Colors.red[600],
+                    width: 100,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                    ),
+                  ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height:100 ,
+                    width: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height:100 ,
+                    width: 100,
+                    color: Colors.green,
+
+                  ),
+                ],
+              ),
+
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.max,
+                    ),
+                  ),
+            ],
+          ),
+        ),
       ),
     ),
   );
